71dfa5c1e8b3cfcb1eef262108fc89cc6b13ec3c
Kevin Lansot <KEVIN.LANSOT@gmail.com>
2023-09-26 11:42:07 +0200

    feat: delete movie and user

--- movieHandlers.js
+++ movieHandlers.js
@@ -96,9 +96,29 @@ const updateMovie = (req, res) => {
   });
 }
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query(
+      "DELETE from movies WHERE id = ?", [id]
+    )
+    .then (([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      res.status(500).send("Error deleting the movie");
+    });
+}
+
 module.exports = {
   getMovies,
   getMovieById,
   postMovie,
-  updateMovie
+  updateMovie,
+  deleteMovie
 };
--- userHandlers.js
+++ userHandlers.js
@@ -70,9 +70,30 @@ const updateUser = (req, res) => {
   });
 }
 
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query(
+      "DELETE from users WHERE id=?", [id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Error deleting the user");
+    })
+}
+
 module.exports = {
   getUsers,
   getUserById,
   postUser,
-  updateUser
+  updateUser,
+  deleteUser
 };
